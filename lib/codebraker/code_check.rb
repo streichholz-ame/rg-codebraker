072536f260d2660a12fad39398775a1d724acd71
Anastasiia <ovcharenko.nastia@gmail.com>
2021-06-25 12:50:53 +0300

    review fix

--- lib/codebraker/code_check.rb
+++ lib/codebraker/code_check.rb
@@ -17,6 +17,8 @@ module Codebraker
       @player_guess.zip(@code)
     end
 
+    private
+
     def exact_match_count
       zip_code.count { |player_digit, secret_digit| player_digit == secret_digit }
     end
