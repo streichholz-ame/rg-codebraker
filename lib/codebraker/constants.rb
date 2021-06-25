cd328cdfbdb4eaad0fde9cee7f3a25b608fbe37c
Anastasiia <ovcharenko.nastia@gmail.com>
2021-06-25 12:50:53 +0300

    review fix

--- lib/codebraker/constants.rb
+++ lib/codebraker/constants.rb
@@ -8,7 +8,6 @@ module Codebraker
       easy: { attempts: 15, hints: 2 },
       medium: { attempts: 10, hints: 1 },
       hard: { attempts: 5, hints: 1 }
-                   .freeze
     }.freeze
   end
 end
